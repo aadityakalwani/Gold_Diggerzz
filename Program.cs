7954df0d9723ce395bfe26c9b028e7eb2727be1b
aadityakalwani <aadityakalwani@gmail.com>
2024-01-25 22:16:46 +0000

    wait does this work

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@
              * you can trade gold and diamonds for $$$ depending on the rates
              * you use your $$$ to buy upgrades for things like your shovel or your employees
              * or you can 'restart' and sacrifice all your $$$ for a better location with better gold payments per day
-             * (like prestige in all the idle miner games i played)
+             * (like prestige in all the idle miner games i played) 
              */
         }
     }
