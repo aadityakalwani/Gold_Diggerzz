47d3f863eb4548858f405bc31fb580c051762f8c
aadityakalwani <aadityakalwani@gmail.com>
2024-01-25 22:16:11 +0000

    initial idea

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace Gold_Diggerzz
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            /*
+             * This is my digging simulator guys
+             * initial ideas:
+             * (initial inspiration: https://replit.com/@AadityaKalwani/Digging-Simulator#main.py)
+             * basically you dig and you get x amount of gold per day with a possibility of getting diamonds too
+             * you can use gold to go to the market and upgrade stuff like your shovel and employees
+             * it follows the gregorian calendar
+             * so for example, on weekends the employees want extra pay and once per few months the stock market crashes and your gold rates plummet
+             * you can trade gold and diamonds for $$$ depending on the rates
+             * you use your $$$ to buy upgrades for things like your shovel or your employees
+             * or you can 'restart' and sacrifice all your $$$ for a better location with better gold payments per day
+             * (like prestige in all the idle miner games i played)
+             */
+        }
     }
-  }
 }
