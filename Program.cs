6d825e774da74ea5a5ca9482296722ff487ef875
aadityakalwani <aadityakalwani@gmail.com>
2024-01-26 19:32:47 +0000

    got a while loop going to keep going

--- Program.cs
+++ Program.cs
@@ -4,53 +4,54 @@ using System.Collections.Generic;
 namespace Gold_Diggerzz
 {
     internal class Program
-    # region idea dumping ground
-        
-    /*
-     * This is my digging simulator guys
-     * initial ideas:
-     * (initial inspiration: https://replit.com/@AadityaKalwani/Digging-Simulator#main.py)
-     * basically you dig and you get x amount of gold per day with a possibility of getting diamonds too
-     * you can use gold to go to the market and upgrade stuff like your shovel and employees
-     * it follows the gregorian calendar
-     * so for example, on weekends the employees want extra pay and once per few months the stock market crashes and your gold rates plummet
-     * you can trade gold and diamonds for $$$ depending on the rates
-     * you use your $$$ to buy upgrades for things like your shovel or your employees
-     * or you can 'restart' and sacrifice all your $$$ for a better location with better gold payments per day
-     * (like prestige in all the idle miner games i played)
-     */
-        
-    /*
-     * basic functionality first:
-     * menu option:
-     * 1 - dig one day
-     * simulates 1 day's worth of digging
-     * 2 - go to market
-     * later, the ability to trade in the market
-     */
-        
-    # endregion
-    
-    /*
-     i plan to use a 2d array to store the resources
-     the resources are:
-         gold
-         diamonds
-         $$$
-         employees
-         etc etc we'll get to the rest later on
-    */
-
-    /* system for managing resources
-     * i have a 2d array for the resources
-     * i can run these into a subroutine that updates the resources
-     * this saves me having to pass in the 2d array every single place where its needed
-     * trouble is i have to pass in arguments and parameters for every single item?
-     * or i can just pass in the "item to change" and its "new value"
-     */
-    
 
     {
+        
+        # region idea dumping ground
+        
+        /*
+         * This is my digging simulator guys
+         * initial ideas:
+         * (initial inspiration: https://replit.com/@AadityaKalwani/Digging-Simulator#main.py)
+         * basically you dig and you get x amount of gold per day with a possibility of getting diamonds too
+         * you can use gold to go to the market and upgrade stuff like your shovel and employees
+         * it follows the gregorian calendar
+         * so for example, on weekends the employees want extra pay and once per few months the stock market crashes and your gold rates plummet
+         * you can trade gold and diamonds for $$$ depending on the rates
+         * you use your $$$ to buy upgrades for things like your shovel or your employees
+         * or you can 'restart' and sacrifice all your $$$ for a better location with better gold payments per day
+         * (like prestige in all the idle miner games i played)
+         */
+        
+        /*
+         * basic functionality first:
+         * menu option:
+         * 1 - dig one day
+         * simulates 1 day's worth of digging
+         * 2 - go to market
+         * later, the ability to trade in the market
+         */
+        
+        # endregion
+    
+        /*
+         i plan to use a 2d array to store the resources
+         the resources are:
+             gold
+             diamonds
+             $$$
+             employees
+             etc etc we'll get to the rest later on
+        */
+
+        /* system for managing resources
+         * i have a 2d array for the resources
+         * i can run these into a subroutine that updates the resources
+         * this saves me having to pass in the 2d array every single place where its needed
+         * trouble is i have to pass in arguments and parameters for every single item?
+         * or i can just pass in the "item to change" and its "new value"
+         */
+        
         public static void Main(string[] args)
         {
             // pregame:
@@ -63,25 +64,29 @@ namespace Gold_Diggerzz
 
         public static void RunGame(Dictionary<string, int> resourceDictionary)
         {
-            int MenuOption = UserMenuOption(resourceDictionary);
-            
-            switch (MenuOption)
+            int menuOption = 0;
+            do
             {
-                case 1:
-                    Console.WriteLine("You have chosen to dig one day");
-                    DigOneDay(resourceDictionary);
-                    break;
-                case 2:
-                    Console.WriteLine("You have chosen to go to the market");
-                    GoToMarket();
-                    break;
-                case 3:
-                    Console.WriteLine("You have chosen to quit the game");
-                    break;
-                default:
-                    Console.WriteLine("Please enter a valid option");
-                    break;
-            }
+                menuOption = UserMenuOption(resourceDictionary);
+            
+                switch (menuOption)
+                {
+                    case 1:
+                        Console.WriteLine("You have chosen to dig one day");
+                        DigOneDay(resourceDictionary);
+                        break;
+                    case 2:
+                        Console.WriteLine("You have chosen to go to the market");
+                        GoToMarket();
+                        break;
+                    case 3:
+                        Console.WriteLine("You have chosen to quit the game");
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid option");
+                        break;
+                }
+            } while (menuOption != 3);
             
         }
 
@@ -93,8 +98,8 @@ namespace Gold_Diggerzz
             Console.WriteLine("2 - Go to market");
             Console.WriteLine("3 - Quit game");
             
-            int UserOption = int.Parse(Console.ReadLine());
-            return UserOption;
+            int userOption = int.Parse(Console.ReadLine());
+            return userOption;
         }
 
         public static void DigOneDay(Dictionary<string,int> resources)
@@ -112,27 +117,20 @@ namespace Gold_Diggerzz
             // 80% chance of finding gold
             Random random = new Random();
             int finalRandom = random.Next(0, 10);
-
-            bool goldFound = false;
-
-            if (finalRandom < 8)
-            {
-                goldFound = true;
-            }
+            
+            bool goldFound = finalRandom < 8;
             
             // update values within the resources dictionary
             if (goldFound)
             {
                 Console.WriteLine("OMG bro you found gold \ud83d\udc8e");
                 resources["Gold"] += 1;
+                
             }
             resources["Dollars"] -= totalWages;
             
-            
-            
             Console.WriteLine($"Your {numberOfEmployees} employees charged a wage of ${totalWages} today." +
                               $"You now have ${resources["Dollars"]}");
-            
             
             PrintResources(resources);
         }
@@ -148,7 +146,6 @@ namespace Gold_Diggerzz
         // because realistically all subroutines already have the resourceDict passed in already
         public static void ManageResources()
         {
-            
         }
         
         public static Dictionary<string,int> CreateResourceDictionary()
@@ -181,13 +178,14 @@ namespace Gold_Diggerzz
             }
         }
 
-        // subroutine i can use to get the daily rates of things liek employee wage
+        // subroutine i can use to get the daily rates of things like employee wage
         public static int GetRates()
         {
-            int EmployeeWage = 10;
+            int employeeWage = 10;
 
-            return EmployeeWage;
+            return employeeWage;
         }
+        
 
     }
 }
